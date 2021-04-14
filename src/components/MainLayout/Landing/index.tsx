179575671a9bb98a717d81fab94f45e9fa499154
Nelson Chinedu <egwuchinedu69@gmail.com>
2021-04-14 23:16:44 +0100

    add trending blog post

--- src/components/MainLayout/Landing/index.tsx
+++ src/components/MainLayout/Landing/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Herosection from 'src/components/MainLayout/Herosection';
+import TrendingPost from 'src/components/MainLayout/TrendingPost';
 
 const Landing = () => {
   return (
     <>
       <Herosection />
+      <TrendingPost />
     </>
   );
 };
