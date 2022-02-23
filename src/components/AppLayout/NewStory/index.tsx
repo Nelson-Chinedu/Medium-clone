dc1758fa3e469e5be9fe2a314e900e6f3de82492
Nelson Chinedu <egwuchinedu69@gmail.com>
2022-02-23 06:19:14 +0100

    remove unused line

--- src/components/AppLayout/NewStory/index.tsx
+++ src/components/AppLayout/NewStory/index.tsx
@@ -1,26 +1,18 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 
 import Navigation from 'src/components/AppLayout/NewStory/Navigation';
 
 const NewStory: FunctionComponent<{}> = () => {
-  const [el, setEl] = useState(0);
-  let attr : any = [];
-  const handleEnter = (e: any) => {
-    if (e.key === 'Enter'){
-      React.createElement('p', {}, 'this is it')
-      // setEl(el + 1);
-      attr.push(1);
-    }
-    console.log(attr.length, '>>')
-  }
   return (
     <div className="w-4/5 mx-auto">
       <Navigation />
-      <div className="w-3/4 mx-auto my-20 border border-blue-400 h-50" contentEditable suppressContentEditableWarning onKeyPress={handleEnter}>
-        
-      </div>
+      <div
+        className="w-3/4 mx-auto my-20 border border-blue-400 h-50"
+        contentEditable
+        suppressContentEditableWarning
+      ></div>
     </div>
-  )
+  );
 };
 
 export default NewStory;
