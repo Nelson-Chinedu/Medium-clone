00d64402696fc6df54ecbd27d818fc406221e0b8
Nelson Chinedu <egwuchinedu69@gmail.com>
2022-03-13 10:26:19 +0100

    fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,13 @@ import React, { FunctionComponent, useState } from 'react';
 
 import Landing from 'src/pages/Main/Landing';
 import Dashboard from 'src/pages/App/Landing';
-import GetStartedContextProvider from 'src/context/GetStarted-ctx';
 
 const Index: FunctionComponent<{}> = () => {
   const [loggedIn] = useState<boolean>(false);
   if (loggedIn) {
     return <Dashboard />;
   }
-  return (
-    <GetStartedContextProvider>
-      <Landing />
-    </GetStartedContextProvider>
-  );
+  return <Landing />;
 };
 
 export default Index;
